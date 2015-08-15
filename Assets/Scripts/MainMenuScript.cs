374b25ea0039bceeca6698a8778d0189ed4b7afd
RandomTroll18 <joshuaarnoldrillera@yahoo.com.ph>
2015-08-15 19:05:59 +1000

    Created scripts and buttons for navigating between scenes. There is a disabled button in the tutorial level which allows you to go back to the main menu. If you want to try it out, be sure to enable it.

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/*
+	 * Function used to load a given level
+	 * 
+	 * Arguments
+	 * - String level - The name of the scene to load
+	 */
+	public void LoadLevel (string level) {
+		Application.LoadLevel(level);
+	}
+
+	/*
+	 * Function used to quit the application
+	 */
+	public void Quit () {
+		Application.Quit();
+	}
+}
