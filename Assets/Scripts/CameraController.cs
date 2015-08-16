34cf91b773cc0ff449cc3fa936ff9434c74e4ed2
Ben Gordon <beng_1995@hotmail.com>
2015-08-16 10:39:27 +1000

    - Changed the CameraController script so it can detect which tile the player clicked. Positions of tiles start at (0,0) (origin point of world)
    SCENE TUTORIAL
    - Removed the hacker object
    - Added a player cube object
    - Updated main camera so it works with the new script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour {
 	public float camSpeed;
 	// The distance the mouse pointer needs to be from the edge before the screen moves.
 	public float GUISize;
+	public LayerMask layerMask;
 	
 	private Vector3 offset;
 	
@@ -15,6 +16,7 @@ public class CameraController : MonoBehaviour {
 	}
 
 	void Update() {
+		// Camera panning with mouse
 		Rect recdown = new Rect (0, 0, Screen.width, GUISize);
 		Rect recup = new Rect (0, Screen.height - GUISize, Screen.width, GUISize);
 		Rect recleft = new Rect (0, 0, GUISize, Screen.height);
@@ -31,6 +33,18 @@ public class CameraController : MonoBehaviour {
 		
 		if (recright.Contains(Input.mousePosition))
 			transform.Translate(camSpeed, 0, 0, Space.World);
+
+		// Mouse click detection
+		if (Input.GetMouseButtonUp (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, Mathf.Infinity, layerMask)) {
+				Debug.Log (hit.point);
+				float x = Mathf.Ceil ((hit.point.x - 1) / 2);
+				float z = Mathf.Ceil ((hit.point.z - 1) / 2);
+				Debug.Log (x + " " + z);
+			}
+		}
 	}
 
 	/*
