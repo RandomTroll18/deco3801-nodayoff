2df589d5e68026c352c981d55e04ba6d2e2e0c89
kenny128 <kenneth-4evr@hotmail.com>
2015-09-20 16:02:53 +1000

    AP Slider text

--- Assets/Scripts/Interactable/SkillSlider.cs
+++ Assets/Scripts/Interactable/SkillSlider.cs
@@ -3,18 +3,23 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class SkillSlider : MonoBehaviour {
+	
+	public Text SliderText;
+	public Slider APSlider;
 
-	string sliderTextString;
-	Text sliderText;
-	
-	void start(){
-		sliderText = GetComponent<Text> ();
+	public void TextUpdate (){
+
+		GameObject Player = GameObject.Find ("Player");
+		Player PlayerScript = Player.GetComponent<Player>();
+
+		double Multiplier = 1;
+		//Multiplier = PlayerScript.GetPlayerClassObject().GetDefaultStat();
+		int actualAP = (int)Mathf.Floor((float)(PlayerScript.GetStatValue(Stat.AP) * (float)APSlider.value / 100f));
+		int multipliedAP = (int)Mathf.Floor((float)actualAP);// * (float)Multiplier);
+		SliderText.text = multipliedAP.ToString();
+
 	}
-	
-	public void textUpdate (float textUpdateNumber){
-		textUpdateNumber.ToString (sliderTextString);
-		sliderText.text = sliderTextString;
-	}
+		
 	// Update is called once per frame
 	void Update () {
 		
--- Assets/Scripts/SkillCheck.cs
+++ Assets/Scripts/SkillCheck.cs
@@ -5,24 +5,13 @@ using System.Collections;
 public class SkillCheck : MonoBehaviour {
 
 	public Slider APSlider;
-	public Text APText;
 	private InteractiveObject Current;
-	private GameObject Player = GameObject.Find ("Player");
-	//private Player PlayerScript = Player.GetComponent<Player>();
 
 	void Start(){
 
 		//GameObject.FindGameObjectWithTag ("Interactable").GetComponent<InteractiveObject>().CloseEvent();
 	}
-
-	void Update(){
-		/*double Multiplier = 1;
-		Multiplier = PlayerScript.GetPlayerClassObject().GetDefaultStat(x);
-		int actualAP = (int)Mathf.Floor((float)(PlayerScript.GetStatValue(Stat.AP) * (float)APSlider.value / 100f));
-		int multipliedAP = (int)Mathf.Floor((float)actualAP * (float)Multiplier);
-		APText.text = multipliedAP;*/
-	}
-
+	
 	public void Action(GameObject x) {
 		x.GetComponent<SkillCheck>().Action();
 	}
