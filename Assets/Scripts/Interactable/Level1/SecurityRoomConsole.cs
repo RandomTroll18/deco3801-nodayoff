a3e5fcce85a948b7fd628d7b47988827cac14d11
kenny128 <kenneth-4evr@hotmail.com>
2015-09-22 11:54:51 +1000

    Security Room Console

---
+++ Assets/Scripts/Interactable/Level1/SecurityRoomConsole.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecurityRoomConsole : InteractiveObject {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public override void TakeAction(int input){
+		
+		if (IsInactivated) {
+			Debug.Log ("Inactive");
+			return;
+		}
+
+		SecurityRoomCard SRC = gameObject.AddComponent<SecurityRoomCard>();
+		GameObject SRCGO = SRC.CreateCard ();
+		this.CloseEvent();
+
+	}
+}
