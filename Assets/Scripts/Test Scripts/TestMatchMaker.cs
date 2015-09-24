fae65535ce199e80f88c805877620a1f28382b77
RandomTroll18 <joshuaarnoldrillera@yahoo.com.ph>
2015-09-24 12:43:05 +1000

    Put in icons for items and effects and added photon unity networking assets. These are the big leagues folks

---
+++ Assets/Scripts/Test Scripts/TestMatchMaker.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+public class TestMatchMaker : Photon.PunBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	void OnGUI() {
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	public override void OnConnectedToPhoton()
+	{
+		Debug.Log("Connected to Photon");
+	}
+
+	public override void OnConnectedToMaster()
+	{
+		Debug.Log("Connected to my master");
+		PhotonNetwork.JoinLobby();
+	}
+
+	public override void OnJoinedRoom()
+	{
+		GameObject cube = PhotonNetwork.Instantiate("TestPrefabs/CubePrefab", Vector3.zero, Quaternion.identity, 0);
+	}
+	
+	public override void OnJoinedLobby()
+	{
+		Debug.Log("Lobby joined");
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed() {
+		Debug.Log("Can't join random room");
+		PhotonNetwork.CreateRoom(null);
+	}
+}
