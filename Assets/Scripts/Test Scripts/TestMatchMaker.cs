5a8025efc822749f1dac60a18bbea48973ac6f13
RandomTroll18 <joshuaarnoldrillera@yahoo.com.ph>
2015-09-28 14:34:16 +1000

    My network tests. Doesn't touch our main scene so don't worry. It does change the main menu and TestScene1 so keep that in mind

--- Assets/Scripts/Test Scripts/TestMatchMaker.cs
+++ Assets/Scripts/Test Scripts/TestMatchMaker.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 public class TestMatchMaker : Photon.PunBehaviour {
 
+	public static string PlayerName; // The name of the player
+	public static string RoomName; // The room that the player wants to join
+
 	// Use this for initialization
 	void Start () {
 		PhotonNetwork.ConnectUsingSettings("0.1");
@@ -9,6 +12,8 @@ public class TestMatchMaker : Photon.PunBehaviour {
 	
 	void OnGUI() {
 		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+		GUILayout.Label("Player Name: " + PlayerName);
+		GUILayout.Label("Room Name: " + RoomName);
 	}
 
 	public override void OnConnectedToPhoton()
@@ -24,13 +29,23 @@ public class TestMatchMaker : Photon.PunBehaviour {
 
 	public override void OnJoinedRoom()
 	{
+		Debug.Log("Joined room: " + PhotonNetwork.room.name);
 		GameObject cube = PhotonNetwork.Instantiate("TestPrefabs/CubePrefab", Vector3.zero, Quaternion.identity, 0);
+		GameObject test = Instantiate<GameObject>(GameObject.CreatePrimitive(PrimitiveType.Capsule));
+		test.GetComponent<Transform>().position = Vector3.zero;
 	}
 	
 	public override void OnJoinedLobby()
 	{
 		Debug.Log("Lobby joined");
-		PhotonNetwork.JoinRandomRoom();
+		if (PlayerName.Length == 0 || RoomName.Length == 0) PhotonNetwork.JoinRandomRoom();
+		else PhotonNetwork.JoinRoom(RoomName);
+
+	}
+
+	void OnPhotonJoinRoomFailed() {
+		Debug.Log("Can't join given room");
+		PhotonNetwork.CreateRoom(RoomName);
 	}
 
 	void OnPhotonRandomJoinFailed() {
---
+++ Assets/Scripts/Test Scripts/TestNetworkMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TestNetworkMenuScript : MonoBehaviour {
+
+	/* Input Fields */
+	public InputField NameInputField;
+	public InputField RoomInputField;
+	public Text ErrorMessageLabelField; // Error message
+
+	/**
+	 * Load the test scene
+	 */
+	public void LoadNetworkTestScene() {
+		if (NameInputField.text.Length == 0) ErrorMessageLabelField.text = "No name given";
+		else if (RoomInputField.text.Length == 0) ErrorMessageLabelField.text = "No room given";
+		else {
+			ErrorMessageLabelField.text = "All fields are good";
+			TestMatchMaker.PlayerName = NameInputField.text;
+			TestMatchMaker.RoomName = RoomInputField.text;
+			Application.LoadLevel("TestScene1");
+		}
+	}
+}
