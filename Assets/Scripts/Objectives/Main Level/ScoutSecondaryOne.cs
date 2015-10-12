9699a7fb475d4deca1b3e9a066b3478e8e884c49
RandomTroll18 <joshuaarnoldrillera@yahoo.com.ph>
2015-10-12 17:57:41 +1000

    Made it so that the Scout's Night Vision Goggle bonus is a status effect, instead of just setting the stat. :P For the record Ben, what you've done would have just been overwritten by InitializePlayer() anyway, so that wouldn't have worked :P

--- Assets/Scripts/Objectives/Main Level/ScoutSecondaryOne.cs
+++ Assets/Scripts/Objectives/Main Level/ScoutSecondaryOne.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class ScoutSecondaryOne : SecondaryObjective {
 
+	Effect visionEffect; // The vision effect
+
 	void Start() {
 		ObjectiveName = "ScoutSecondaryOne";
 		Title = "Find the night vision goggles";
@@ -10,12 +12,17 @@ public class ScoutSecondaryOne : SecondaryObjective {
 			"An Engineer will be helpful";
 
 		GameObject.Find("Scout Secondary One").AddComponent<ScoutSecondaryOneInteractable>().StartMe();
+
+		visionEffect = new StatusTurnEffect(Stat.VISION, 3.0, 1, 
+				"Night Vision Goggles", "Icons/Effects/DefaultEffect", -1, true);
 	}
 
 	public override void OnComplete() {
 		// not really tested
-		// applying a turn effect would be nicer
-		Player.MyPlayer.GetComponent<Player>().SetStatValue(Stat.VISION, 3);
+		// applying a turn effect would be nicer.  
+		// Yes it would. Here you go: - Josh
+		Player.MyPlayer.GetComponent<Player>().AttachTurnEffect(visionEffect);
+		//Player.MyPlayer.GetComponent<Player>().SetStatValue(Stat.VISION, 3);
 		Destroy(this);
 	}
 }
